a6b968d384374585a629fd42e6fca43fc414578c
omar <omar@gmail.com>
2023-07-14 13:22:47 -0700

    p,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 /**
  * main - Entry point
- * Description: puts function
+ *
+ * Description: A C program that prints with put function
+ *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
