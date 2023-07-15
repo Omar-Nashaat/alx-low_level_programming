4dd563d8b28bf1c6a0fb896a69ec97a185479498
omar <omar@gmail.com>
2023-07-14 19:09:20 -0700

    r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print the alpha without q and e
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+char start = 'a';
+char end = 'z';
+char ch = start;
+
+	while (ch <= end)
+	{
+	if (ch != 'q' && ch != 'e')
+	putchar(ch);
+	ch++;
+	}
+	return (0);
+}
