c1636fddc0d8910cafec49f029382c64b862c2e0
omar <omar@gmail.com>
2023-07-14 17:28:29 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program prints the alpha in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+	char ch = start;
+
+	while (ch != end)
+	{
+	putchar(ch);
+	ch++
+	}
+	return (0);
+}
