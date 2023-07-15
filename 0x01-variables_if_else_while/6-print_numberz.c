3878e164000576a8c534f058adad350d8530f27d
omar <omar@gmail.com>
2023-07-14 19:24:26 -0700

    y

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print the alpha without q and e
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchat(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
