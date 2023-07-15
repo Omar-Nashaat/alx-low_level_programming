c289089d9398f8a5391385201f81e4b1097df804
omar <omar@gmail.com>
2023-07-14 19:16:21 -0700

    t

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print the alpha without q and e
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
