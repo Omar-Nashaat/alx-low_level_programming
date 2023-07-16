fa9117a3cd2be8344fa38aa4f0310f51cd1cfb61
omar <omar@gmail.com>
2023-07-16 12:12:36 -0700

    q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 
 /**
- * mian -entry point
+ * main - entry point
  *
- * Description: A C program that prints text without printf and putchar
+ * Description: A C program that prints text without printf and putchar using library unistdand array and function write that takes the start of printing and the array and the number of letters will be printed as parameters
  *
  * Return: Always 1 (fail)
 */
 
 int main(void)
 {
-	char qou[] = "and that piece of art is useful\" - Dora Korpar, 20-5-10-19\n";
+	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, qou, 59);
 	return (1);
