785efc57e716a4c60a99cc95bde8d46447c322cf
omar <omar@gmail.com>
2023-07-18 15:40:31 -0700

    w

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -14,7 +14,7 @@ int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	else if (c >= 48 && c <= 57)
+	else if (c >= 48 && c < 58)
 		return (1);
 	else if (c >= 97 && c <= 122)
 		return (1);
