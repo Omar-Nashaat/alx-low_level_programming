56e7c8786dfa033fb45a46999dad9fdb75ab242a
omar <omar@gmail.com>
2023-07-20 16:22:58 -0700

    e

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, 
+ * using _putchar twice
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9;i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
