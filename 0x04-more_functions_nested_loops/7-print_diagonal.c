687b69e21ed496886f47a1c9b3f4f919b15f3351
omar <omar@gmail.com>
2023-07-20 17:23:43 -0700

    y

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,7 +19,7 @@ void print_diagonal(int n)
 		for (i = 1; i <= n; i++)
 		{
 			for (space = 1; space <= i; space++)
-				_putchar('\n');
+				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
 		}
