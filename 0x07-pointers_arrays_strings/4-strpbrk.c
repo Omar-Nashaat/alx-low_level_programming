cc0dd0f1cf1f42bf5d0ff693ed3f9450e814cee2
omar <omar@gmail.com>
2023-07-31 18:32:35 -0700

    h

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,9 +9,9 @@ char *_strpbrk(char *s, char *accept)
 {
 int k;
 
-		while (*s >= '\0')
+		while (*s)
 		{
-			for (k = 0; accept[k] >= '\0'; k++)
+			for (k = 0; accept[k]; k++)
 			{
 			if (*s == accept[k])
 			return (s);
