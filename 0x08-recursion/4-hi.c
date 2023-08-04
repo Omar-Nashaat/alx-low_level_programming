6be4b747c57bab812e64ed96135770553a0edc18
root <root@LAPTOP-PP6JIM4A.localdomain>
2023-08-04 05:01:55 +0300

    h

---
+++ 0x08-recursion/4-hi.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * factorial - returns the factorial of a given number.
+ *
+ * @n: input
+ *
+ *Return: integer
+ */
+
+void print(void)
+{
+    printf("hi");
+    return (0);
+}
