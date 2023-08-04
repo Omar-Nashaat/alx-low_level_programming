ee9d339c3fa6326fd0434ad7b36b3f248b83cb6c
root <root@LAPTOP-PP6JIM4A.localdomain>
2023-08-04 16:56:11 +0300

    g

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints program name , then new line
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
