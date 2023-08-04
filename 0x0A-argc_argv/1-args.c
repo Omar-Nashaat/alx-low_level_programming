e3ca1e03d68ddf17e7b7a0bc1e8aa7bcbc3feadf
root <root@LAPTOP-PP6JIM4A.localdomain>
2023-08-04 17:16:53 +0300

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of string carries arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d", argc - 1);
+return (0);
+}
