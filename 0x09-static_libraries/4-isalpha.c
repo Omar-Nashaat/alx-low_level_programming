1dd06ea26597d14a4882b6e886a7a30ea4e221c5
root <root@LAPTOP-PP6JIM4A.localdomain>
2023-08-04 13:13:35 +0300

    g

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isalpha - function to check if
+ *character is lowercase or uppercase
+ *
+ *@c: checks input of function
+ *
+ *Return: return 1 if c is a letter,
+ *lowercase or uppercase Returns 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write the char c to stdout
+ * @c: the char to print
+ *
+ * Return: on success 1.
+ *	on error, -1 is retured, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
